9e8402b2b5981588a081920914bd210d04aef968
BuxGuerra <guilhermebuxguerra@gmail.com>
2025-01-16 18:59:39 -0300

    just for local testing / ignore

--- src/test.py
+++ src/test.py
@@ -2,6 +2,14 @@ import igraph as ig
 import tsplib95
 import math
 
+
+
+
+##IGNORA ISSO AQUI / FOI SÓ PRA UNS TESTES LOCAIS
+
+
+
+
 # Função para calcular a distância Euclidiana
 def euclidean_distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
